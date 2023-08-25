71a083a7ddf030b32c14be92a57b0426da41683e
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2023-08-25 17:42:13 -0400

    cleaned out audio; improved test coverage; wav2af is polymorphic over bytes buffer filepath

--- src/moshi/utils/audio.py
+++ src/moshi/utils/audio.py
@@ -1,19 +1,20 @@
-""" This module provide audio processing utilities. """
+""" This module provides audio processing utilities:
+- wav2af: convert a wav file to an AudioFrame
+- energy: calculate the RMS energy of an audio frame
+- seconds: calculate the length in seconds of an audio frame
+"""
 import io
 from pathlib import Path
 from textwrap import shorten
 
 import av
 import numpy as np
-from av import AudioFifo, AudioFormat, AudioFrame, AudioLayout
+from av import AudioFifo, AudioFrame
 from loguru import logger
 
 from . import wavfile
 
-logger.success("Loaded!")
-
-
-def get_frame_energy(af: AudioFrame) -> float:
+def energy(af: AudioFrame) -> float:
     """Calculate the RMS energy of an audio frame."""
     arr = af.to_ndarray()  # produces array with dtype of int16
     # NOTE int16 is too small for squares of typical signal stregth so int32 is used
@@ -23,61 +24,14 @@ def get_frame_energy(af: AudioFrame) -> float:
     return energy
 
 
-def get_frame_seconds(af: AudioFrame) -> float:
+def seconds(af: AudioFrame) -> float:
     """Calculate the length in seconds of an audio frame."""
     seconds = af.samples / af.rate
     return seconds
 
 
-def get_frame_start_time(frame) -> float:
-    """Get the clock time (relative to the start of the stream) at which the frame should start"""
-    return frame.pts / frame.rate
-
-
-# def empty_frame(
-#     length=128, format=AUDIO_FORMAT, layout=AUDIO_LAYOUT, rate=SAMPLE_RATE, pts=None
-# ) -> AudioFrame:
-#     fmt = AudioFormat(format)
-#     lay = AudioLayout(layout)
-#     size = (len(lay.channels), length)
-#     samples = np.zeros(size, dtype=np.int16)
-#     if not fmt.is_planar:
-#         samples = samples.reshape(1, -1)
-#     frame = AudioFrame.from_ndarray(samples, format=format, layout=layout)
-#     frame.rate = rate
-#     frame.pts = pts
-#     return frame
-
-
-def write_audio_frame_to_wav(frame: AudioFrame, output_file):
-    # Source: https://stackoverflow.com/a/56307655/5298555
-    with av.open(output_file, "w") as container:
-        stream = container.add_stream("pcm_s16le")
-        for packet in stream.encode(frame):
-            container.mux(packet)
-        for packet in stream.encode(None):
-            container.mux(packet)
-    logger.debug(f"Wrote audio in WAV (pcm_s16le) format to {output_file}")
-
-
-def write_bytes_to_wav_file(filename: str, bytestring: bytes):
-    with open(filename, "wb") as f:
-        f.write(bytestring)
-
-
-def load_wav_to_buffer(fp: str) -> AudioFifo:
-    with av.open(fp, "r") as container:
-        fifo = AudioFifo()
-        for frame in container.decode(audio=0):
-            fifo.write(frame)
-    return fifo
-
-
-def wavb2af(wav: bytes) -> AudioFrame:
-    """Parse wav file format, returning an AudioFrame of the body."""
-    assert isinstance(wav, bytes)
-    buf = io.BytesIO(wav) 
-    sample_rate, arr = wavfile.read(buf)
+def _wavb2af(wav: io.BytesIO) -> AudioFrame:
+    sample_rate, arr = wavfile.read(wav)
     samples, channels = arr.shape
     layout = "stereo" if channels == 2 else "mono"
     assert channels == len(av.AudioLayout(layout).channels)
@@ -92,7 +46,19 @@ def wavb2af(wav: bytes) -> AudioFrame:
     logger.debug(f"af={af}")
     return af
 
-def wav2af(waf: Path) -> AudioFrame:
+def _wavp2af(waf: Path) -> AudioFrame:
     with open(waf, "rb") as f:
-        wav = f.read()
-    return wavb2af(wav)
+        wavb = f.read()
+    return _wavb2af(wavb)
+
+def wav2af(wav: bytes | io.BytesIO | Path):
+    """Convert a wav file to an AudioFrame."""
+    if isinstance(wav, bytes):
+        wav = io.BytesIO(wav)
+        return _wavb2af(wav)
+    elif isinstance(wav, io.BytesIO):
+        return _wavb2af(wav)
+    elif isinstance(wav, Path):
+        return _wavp2af(wav)
+    else:
+        raise TypeError(f"wav must be bytes, io.BytesIO, or Path, not {type(wav)}")
--- tests/test_audio.py
+++ tests/test_audio.py
@@ -1,8 +1,28 @@
+import av
+
 from moshi.utils import audio
 
-def test_wav(wavbytes):
-    af = audio.wavb2af(wavbytes)
-    audio.write_audio_frame_to_wav(af, "test.wav")
+def write_audio_frame_to_wav(frame: av.AudioFrame, output_file):
+    # Source: https://stackoverflow.com/a/56307655/5298555
+    with av.open(output_file, "w") as container:
+        stream = container.add_stream("pcm_s16le")
+        for packet in stream.encode(frame):
+            container.mux(packet)
+        for packet in stream.encode(None):
+            container.mux(packet)
+    print(f"Wrote WAV (pcm_s16le): {output_file}")
 
+
+def test_wav(wavbytes):
+    af = audio.wav2af(wavbytes)
+    write_audio_frame_to_wav(af, "test.wav")
+
+def test_seconds(wavbytes):
+    af = audio.wav2af(wavbytes)
+    assert 0.5 < audio.seconds(af) < 1.5, "Saying 'hello' should take ~1 second"
+
+def test_energy(wavbytes):
+    af = audio.wav2af(wavbytes)
+    _ = audio.energy(af)
 
 # def test_m4a(m4abytes):
