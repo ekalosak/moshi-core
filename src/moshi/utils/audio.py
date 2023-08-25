88e346f7efd8e8235258a09e878ad783e95823cc
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2023-08-25 17:18:17 -0400

    parsing wav buffer

--- src/moshi/utils/audio.py
+++ src/moshi/utils/audio.py
@@ -1,4 +1,5 @@
 """ This module provide audio processing utilities. """
+import io
 from pathlib import Path
 from textwrap import shorten
 
@@ -7,11 +8,7 @@ import numpy as np
 from av import AudioFifo, AudioFormat, AudioFrame, AudioLayout
 from loguru import logger
 
-# defaults
-SAMPLE_RATE = 48000
-AUDIO_FORMAT = "s16"
-AUDIO_LAYOUT = "stereo"
-logger.info(f"Audio defaults: {SAMPLE_RATE}Hz {AUDIO_FORMAT} {AUDIO_LAYOUT}")
+from . import wavfile
 
 logger.success("Loaded!")
 
@@ -37,19 +34,19 @@ def get_frame_start_time(frame) -> float:
     return frame.pts / frame.rate
 
 
-def empty_frame(
-    length=128, format=AUDIO_FORMAT, layout=AUDIO_LAYOUT, rate=SAMPLE_RATE, pts=None
-) -> AudioFrame:
-    fmt = AudioFormat(format)
-    lay = AudioLayout(layout)
-    size = (len(lay.channels), length)
-    samples = np.zeros(size, dtype=np.int16)
-    if not fmt.is_planar:
-        samples = samples.reshape(1, -1)
-    frame = AudioFrame.from_ndarray(samples, format=format, layout=layout)
-    frame.rate = rate
-    frame.pts = pts
-    return frame
+# def empty_frame(
+#     length=128, format=AUDIO_FORMAT, layout=AUDIO_LAYOUT, rate=SAMPLE_RATE, pts=None
+# ) -> AudioFrame:
+#     fmt = AudioFormat(format)
+#     lay = AudioLayout(layout)
+#     size = (len(lay.channels), length)
+#     samples = np.zeros(size, dtype=np.int16)
+#     if not fmt.is_planar:
+#         samples = samples.reshape(1, -1)
+#     frame = AudioFrame.from_ndarray(samples, format=format, layout=layout)
+#     frame.rate = rate
+#     frame.pts = pts
+#     return frame
 
 
 def write_audio_frame_to_wav(frame: AudioFrame, output_file):
@@ -78,36 +75,20 @@ def load_wav_to_buffer(fp: str) -> AudioFifo:
 
 def wavb2af(wav: bytes) -> AudioFrame:
     """Parse wav file format, returning an AudioFrame of the body."""
-    # https://docs.fileformat.com/audio/wav/
-    logger.debug(f"len(wav)={len(wav)}")
     assert isinstance(wav, bytes)
-    assert len(wav) > 0, "Empty wav file."
-    header, body = wav[0:44], wav[44:]
-    logger.debug(f"len(body)={len(body)}")
-    # validate header
-    assert header.startswith(b"RIFF"), "Invalid wav header."
-    assert int.from_bytes(header[4:8], "little") == len(wav) - 8, "File size mismatch, header invalid."
-    assert header[8:12] == b"WAVE", "Invalid wav header."
-    assert header[12:16] == b"fmt ", "Invalid wav header."
-    fmt_len = int.from_bytes(header[16:20], "little")
-    assert fmt_len == 16, "Only PCM format is supported."
-    channels = int.from_bytes(header[22:24], "little")
-    sample_rate = int.from_bytes(header[24:28], "little")
-    bits_per_sample = int.from_bytes(header[34:36], "little")
-    # sample_format = f'flt{bits_per_sample}'  # 'flt32' for 32-bit floating-point samples
-    # audio_format = av.AudioFormat(sample_rate=sample_rate, layout=f'stereo{channels}', format=sample_format)
-    assert channels in [1, 2], "Only mono and stereo wav files are supported."
-    assert header[36:40] == b"data"
-    data_size = int.from_bytes(header[40:44], "little")
-    assert len(body) > 0, "Empty wav body."
-    assert len(body) % 8 == 0
-    arr = np.frombuffer(body, dtype=np.float32)
-    arr = arr.reshape(channels, -1)
-    layout = 'stereo' if channels == 2 else 'mono'
-    logger.debug(f"channels={channels} sample_rate={sample_rate} bit_per_sample={bits_per_sample} data_size={data_size}")
-    # afmt = av.AudioFormat(sample_rate=sample_rate, layout=f'stereo{channels}', format=sample_format)
-    af = AudioFrame.from_ndarray(arr, format=AUDIO_FORMAT, layout=layout)
-    logger.debug(f"af={af}")
+    buf = io.BytesIO(wav) 
+    sample_rate, arr = wavfile.read(buf)
+    samples, channels = arr.shape
+    layout = "stereo" if channels == 2 else "mono"
+    assert channels == len(av.AudioLayout(layout).channels)
+    format = av.AudioFormat("s16")
+    # logger.debug(f"sample_rate={sample_rate}, samples={samples}, channels={channels}, layout={layout}, planar={format.is_planar}")
+    with logger.contextualize(sample_rate=sample_rate, samples=samples, channels=channels, layout=layout, planar=format.is_planar):
+        try:
+            af = AudioFrame.from_ndarray(arr.ravel().reshape(1, -1), format='s16', layout=layout)
+            logger.debug(f"af={af}")
+        except:
+            logger.error(f"Failed to create AudioFrame from wav bytes (as hex): {shorten(wav.hex(), 100)}")
     return af
 
 def wav2af(waf: Path) -> AudioFrame:
