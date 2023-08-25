029acf4e571254cd9e29a06cd0005ff09feb2953
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2023-08-25 18:23:47 -0400

    moved a singular object to module scope

--- src/moshi/utils/storage.py
+++ src/moshi/utils/storage.py
@@ -4,18 +4,17 @@ from loguru import logger
 from moshi import GOOGLE_PROJECT
 from moshi.core.base import Profile
 
-logger.debug("Creating Firestore client...")
-firestore_client = firestore.AsyncClient(project=GOOGLE_PROJECT)
-logger.info(f"Firestore client initialized.")
+client = firestore.AsyncClient(project=GOOGLE_PROJECT)
+profiles = client.collection("profiles")
 
+logger.success("Storage module loaded.")
 
 async def get_profile(uid: str) -> Profile:
     """Get the user profile."""
-    collection_ref = firestore_client.collection("profiles")
-    doc_ref = collection_ref.document(uid)
+    doc_ref = profiles.document(uid)
     doc = await doc_ref.get()
     if not doc.exists:
         raise ValueError(f"User profile not found: {uid}")
     profile = Profile(**doc.to_dict(), uid=uid)
-    logger.trace(f"User profile: {profile}")
+    logger.trace(f"profile={profile}")
     return profile
