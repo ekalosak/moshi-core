bc5d3d03684155e7e109f76ae76697b0c4faeb22
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2023-09-11 17:27:11 -0400

    23.9.7, just a version bump

--- src/moshi/__init__.py
+++ src/moshi/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "23.9.6"
+__version__ = "23.9.7"
 
 import os
 GCLOUD_PROJECT = os.getenv("GCLOUD_PROJECT", "moshi-3")
@@ -7,7 +7,7 @@ from .utils import log
 log.setup_loguru()
 from loguru import logger
 logger.trace("Moshi loading...")
-logger.info(f"GCLOUD_PROJECT={GCLOUD_PROJECT}")
+logger.info(f"GCLOUD_PROJECT={GCLOUD_PROJECT} moshi-core={__version__}")
 
 from .base import *
 from .exceptions import *
