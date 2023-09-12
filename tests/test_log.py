6877a516a5ed03d7c591699788e90811099dd260
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2023-09-12 15:16:31 -0400

    solid logging

--- tests/test_log.py
+++ tests/test_log.py
@@ -10,15 +10,15 @@ from moshi.utils import log
 def test_setup():
     log.setup_loguru()
 
-def _setup(fmt=None, sink=None):
+def _setup(fmt="", sink=print):
     print("RUN SETUP")
     log.setup_loguru(fmt, sink)
     print("DONE")
 
-@pytest.mark.parametrize("fmt", [None, "", "json", "rich"])
+@pytest.mark.parametrize("fmt", ["", "json", "rich"])
 def test_formatting(fmt: str):
     loguru.logger.debug("before")
-    _setup()
+    _setup(fmt=fmt)
     loguru.logger.debug("after")
 
 @pytest.mark.parametrize("sink", [sys.stdout.write, sys.stderr.write, print])
@@ -29,14 +29,19 @@ def test_sink(sink):
 
 @pytest.mark.parametrize("serialize_me", [None, "", 1, 1.0, True, [], {}, {"foo": "bar"}, datetime.now()])
 def test_json_serialization(serialize_me):
+    """Defines the supported loggable objects that we can log to GCP logging."""
     _log = []
     def sink(x):
         _log.append(x)
     _setup(sink=sink, fmt="json")
     with loguru.logger.contextualize(payload=serialize_me):
         loguru.logger.debug("test")
-    # parse json
-    # retrieve foo from extra
     assert len(_log) == 1, "sent more than one log message"
-    print(_log)
     rec = json.loads(_log[0])
+    extra = json.loads(rec["extra"])
+    if isinstance(serialize_me, datetime):
+        payload = datetime.fromisoformat(extra['payload']).isoformat()
+        assert payload == serialize_me.isoformat() + "+00:00", "failed to serialize payload"
+    else:
+        payload = extra['payload']
+        assert payload == serialize_me, "failed to serialize payload"
