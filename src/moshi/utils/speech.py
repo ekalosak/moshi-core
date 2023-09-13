4f002e83451e8294bc0be13e1bba99a495becf65
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2023-09-13 17:44:37 -0400

    voice get tries to get from firebase first; tests to ensure that works

--- src/moshi/utils/speech.py
+++ src/moshi/utils/speech.py
@@ -1,12 +1,13 @@
 """This module provides speech synthesis and transcription utilities.
 """
 import os
-from pathlib import Path
+import random
 from textwrap import shorten
 
 import av
 from google.cloud import speech as stt
 from google.cloud import texttospeech as tts
+from firebase_admin.firestore import firestore
 from loguru import logger
 
 from moshi import GCLOUD_PROJECT
@@ -17,14 +18,19 @@ GOOGLE_SPEECH_SYNTHESIS_TIMEOUT = int(os.getenv("GOOGLE_SPEECH_SYNTHESIS_TIMEOUT
 GOOGLE_VOICE_SELECTION_TIMEOUT = int(os.getenv("GOOGLE_VOICE_SELECTION_TIMEOUT", 5))
 logger.info(f"GOOGLE_SPEECH_SYNTHESIS_TIMEOUT={GOOGLE_SPEECH_SYNTHESIS_TIMEOUT} GOOGLE_VOICE_SELECTION_TIMEOUT={GOOGLE_VOICE_SELECTION_TIMEOUT}")
 
+logger.trace('[START] Loading clients...')
 sclient = stt.SpeechClient()
 client = tts.TextToSpeechClient()
+db = firestore.Client(project=GCLOUD_PROJECT)
+logger.trace('[END] Loading clients...')
 
 class TranscriptionError(Exception):
     """Raised when transcription fails."""
     pass
 
 def gender_match(g1: str, g2: tts.SsmlVoiceGender) -> bool:
+    if g1.lower() not in ("male", "female"):
+        raise ValueError(f"SSML Gender not yet supported: {g1}")
     if g1.lower() == "female" and g2 == 2:
         return True
     elif g1.lower() == "male" and g2 == 1:
@@ -34,38 +40,63 @@ def gender_match(g1: str, g2: tts.SsmlVoiceGender) -> bool:
 
 
 @traced
-def list_voices(lan: str=None) -> list[tts.Voice]:
-    """List all voices supported by the API.
+def list_voices(bcp47: str) -> list[tts.Voice]:
+    """List all voices supported by ChatMoshi. Retrieve them from the Firebase document /config/voices.
+    If that doc doesn't exist, then list all voices from Google Cloud Text-to-Speech.
     Args:
         - lan: if provided, filter by language code. It must be a BCP 47 language code e.g. "en-US" https://www.rfc-editor.org/rfc/bcp/bcp47.txt
     """
-    if lan:
-        logger.debug(f"Listing voices for language: {lan}")
-        response = client.list_voices(language_code=lan, timeout=GOOGLE_VOICE_SELECTION_TIMEOUT)
-    else:
-        logger.debug("Listing all voices.")
-        response = client.list_voices(timeout=GOOGLE_VOICE_SELECTION_TIMEOUT)
-    return response.voices
+    logger.debug(f"Listing voices for language: {bcp47}")
+    try:
+        doc = db.collection("config").document("voices").get()
+        assert doc.exists, "Voices document doesn't exist."
+        _voices = doc.to_dict()[bcp47]
+        voices = [tts.Voice(
+            name=name,
+            ssml_gender=model['ssml_gender'],
+            natural_sample_rate_hertz=24000,
+        ) for name, model in _voices.items()]
+    except (IndexError, AssertionError) as exc:
+        logger.error(f"Failed to get voices from Firebase: {exc}")
+        response = client.list_voices(language_code=bcp47, timeout=GOOGLE_VOICE_SELECTION_TIMEOUT)
+        voices = response.voices
+    return voices
 
-def get_voice(langcode: str, gender="FEMALE", model="Standard") -> str:
+def get_voice(bcp47: str, gender="FEMALE", model="Standard", random_choice=True) -> str:
     """Get a valid voice for the language. Just picks the first match.
     Args:
-        - langcode: in BPC 47 format e.g. "en-US" https://www.rfc-editor.org/rfc/bcp/bcp47.txt
+        - bcp47: Language code in BPC 47 format e.g. "en-US" https://www.rfc-editor.org/rfc/bcp/bcp47.txt
+        - gender: SSML Gender
+        - model: Voice model class (e.g. "Standard", "WaveNet")
+        - random_choice: if True, pick a random voice from the list of matches. If False, pick the first match.
     Raises:
         - ValueError if no voice found.
     Source:
         - https://cloud.google.com/text-to-speech/pricing for list of valid voice model classes
     """
-    logger.debug(f"Getting voice for lang code: {langcode}")
-    voices = list_voices(langcode)
-    logger.debug(f"Language {langcode} has {len(voices)} supported voices.")
+    logger.debug(f"Getting voice for: {bcp47}")
+    voices = list_voices(bcp47)
+    logger.debug(f"Language {bcp47} has {len(voices)} supported voices.")
+    model_matches = []
     for voice in voices:
-        if model in voice.name and gender_match(gender, voice.ssml_gender):
-            logger.debug("Found match")
-            return voice
-    raise ValueError(
-        f"Voice not found for langcode={langcode}, gender={gender}, model={model}"
-    )
+        if model.lower() in voice.name.lower():
+            model_matches.append(voice)
+    if len(model_matches) == 0:
+        logger.warning(f"No voice found for model={model}, using any model.")
+        model_matches = voices
+    gender_matches = []
+    for voice in model_matches:
+        if gender_match(gender, voice.ssml_gender):
+            gender_matches.append(voice)
+    if len(gender_matches) == 0:
+        logger.warning(f"No voice found for gender={gender}, using any model.")
+        gender_matches = model_matches
+    voices = gender_matches
+    if len(voices) > 0:
+        voice = random.choice(voices)
+        logger.debug(f"Found voice: {voice}")
+        return voice
+    raise ValueError(f"Voice not found for {bcp47}, gender={gender}, model={model}")
 
 
 def _synthesize_bytes(text: str, voice: tts.Voice, rate: int = 24000) -> bytes:
--- tests/test_speech.py
+++ tests/test_speech.py
@@ -1,15 +1,36 @@
-import asyncio
+import os
 
+from google.cloud import texttospeech as tts
 import pytest
 
 from moshi.utils import audio, speech
 
 @pytest.mark.gcp
-@pytest.mark.aio
 def test_synthesize():
     msg = "Hello"
-    loop = asyncio.get_event_loop()
-    voice = loop.run_until_complete(speech.get_voice("en-US"))
-    af = loop.run_until_complete(speech.synthesize(msg, voice))
+    voice = speech.get_voice("en-US")
+    af = speech.synthesize(msg, voice)
     assert af.rate == 24000
     print(f"Test wav length: {audio.seconds(af)}")
+
+@pytest.mark.fs
+@pytest.mark.parametrize("bcp47", ["es-MX", "zh-Hans-CN", "en-US", "yue-Hant-HK", "ar-AE"])
+def test_list_voices(bcp47):
+    if not os.environ.get("FIRESTORE_EMULATOR_HOST"):
+        pytest.skip("Skipping test because emulator not running; set the FIRESTORE_EMULATOR_HOST environment variable to run this test.")
+    voices = speech.list_voices(bcp47)
+    assert len(voices) > 0
+    assert all(isinstance(v, tts.Voice) for v in voices)
+
+@pytest.mark.fs
+@pytest.mark.parametrize("bcp47", ["es-MX", "zh-Hans-CN", "en-US", "yue-Hant-HK", "ar-AE"])
+@pytest.mark.parametrize("ssml", ["MALE", "FEMALE"])
+def test_get_voice(bcp47, ssml):
+    if not os.environ.get("FIRESTORE_EMULATOR_HOST"):
+        pytest.skip("Skipping test because emulator not running; set the FIRESTORE_EMULATOR_HOST environment variable to run this test.")
+    voice = speech.get_voice(bcp47)
+    assert isinstance(voice, tts.Voice)
+    if bcp47.startswith("zh"):
+        assert voice.name.startswith('cmn-CN'), "Language mismatch"
+    else:
+        assert voice.name.startswith(bcp47.split('-')[0]), "Language mismatch"
