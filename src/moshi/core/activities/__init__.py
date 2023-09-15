1b554ea856194d713e8efb9f474836ea69dcb5d7
Eric Kalosa-Kenyon <ekalosak@gmail.com>
2023-09-15 08:26:50 -0400

    init lesson activity; added to new() in activity/__init__.py

--- src/moshi/core/activities/__init__.py
+++ src/moshi/core/activities/__init__.py
@@ -1,17 +1,20 @@
 from enum import Enum
 from loguru import logger
 from .core import BaseActivity, sample_activity_id
+from .lesson import Lesson
 from .unstructured import Unstructured
 
 class ActivityType(str, Enum):
     UNSTRUCTURED = "unstructured"  # talk about anything, user-driven, random topic.
     # TOPICAL = "topical"  # talk about a specific topic e.g. sports, politics, etc.
-    # LESSON = "lesson"  # e.g. (beginner, introductions), (intermediate, small talk), (advanced, debate), play out a scenario e.g. a job interview, ordering coffee, etc.
+    LESSON = "lesson"  # e.g. (beginner, introductions), (intermediate, small talk), (advanced, debate), play out a scenario e.g. a job interview, ordering coffee, etc.
 
 def new(activity_type, usr) -> BaseActivity:
     """Create a new activity for the user."""
     if activity_type == ActivityType.UNSTRUCTURED:
         return Unstructured(user=usr)
+    elif activity_type == ActivityType.LESSON:
+        return Lesson(user=usr)
     else:
         raise ValueError(f"Invalid activity type: {activity_type}")
 
---
+++ src/moshi/core/activities/lesson.py
@@ -0,0 +1,84 @@
+from loguru import logger
+from pydantic import BaseModel
+
+from moshi import Message, Role
+from moshi.utils import lang
+from moshi.core.activities import BaseActivity
+
+class Criterion(BaseModel):
+    body: str
+    points: int
+
+class Goal(BaseModel):
+    title: str
+    criteria: list[Criterion]
+
+class Translation(BaseModel):
+    goals: list[Goal]
+    title: str
+    user_prompt: str
+    character_prompt: str
+
+class Lesson(BaseActivity):
+    @property
+    def goals(self, language: str=None) -> list[Goal]:
+        language = language or self.user.language
+        logger.debug(f"json goals={self._translations[language]['goals']}")
+        return [Goal(**g) for g in self._translations[language]["goals"]]
+    
+    @property
+    def title(self, language: str=None) -> str:
+        language = language or self.user.language
+        return self._translations[language]["title"]
+
+    @property
+    def user_prompt(self, language: str=None) -> str:
+        language = language or self.user.language
+        return self._translations[language]["user_prompt"]
+
+    @property
+    def character_prompt(self, language: str=None) -> str:
+        language = language or self.user.language
+        return self._translations[language]["character_prompt"]
+
+    def _translate_activity(self):
+        """Translate the goals, title, and user_prompt. Does NOT write to Firestore."""
+        # NOTE assumes the activity is initially written in us-EN
+        logger.debug(f"_translations={self._translations}")
+        enUS = Translation(**self._translations["en-US"])
+        title = lang.translate_text(enUS.title, self.user.language)
+        user_prompt = lang.translate_text(enUS.user_prompt, self.user.language)
+        character_prompt = lang.translate_text(enUS.character_prompt, self.user.language)
+        goals = []
+        for goal in enUS.goals:
+            title = lang.translate_text(goal.title, self.user.language)
+            criteria = []
+            if len(goal.criteria) > 0:
+                for cri in goal.criteria:
+                    translated_body = lang.translate_text(cri.body, self.user.language)
+                    criteria.append(Criterion(body=translated_body, points=cri.points))
+            goals.append(Goal(title=title, criteria=criteria))
+        self._translations[self.user.language] = Translation(
+            goals=goals, title=title, user_prompt=user_prompt, character_prompt=character_prompt
+        ).model_dump()
+        self.doc.set({"translations": self._translations}, merge=True)
+
+    @property
+    def prompt(self) -> list[Message]:
+        """Return the prompt for the lesson, to be used for LLM completions."""
+        msgs = []
+        for goal in self.goals:
+            msgs.append(Message(
+                role=Role.SYS,
+                body=goal.model_dump_json(),
+            ))
+        msgs.append(Message(
+            role=Role.SYS,
+            body=self.user_prompt,
+        ))
+        msgs.append(Message(
+            role=Role.SYS,
+            body=self.character_prompt,
+        ))
+        logger.debug(f"prompt={msgs}")
+        return msgs
